d29fbe01b74a6bd757740047c04adaf5740deaac
sahil-shahare <sahilshahare380@gmail.com>
2026-01-27 00:07:44 +0530

    Take User Input - @RequestParam

--- Amazon/src/main/java/com/Amazon/Controller/UserController.java
+++ Amazon/src/main/java/com/Amazon/Controller/UserController.java
@@ -1,20 +1,37 @@
 package com.Amazon.Controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.Amazon.Entity.User;
 
 @RestController
-@RequestMapping("/user")
+@RequestMapping("/users")
 public class UserController {
 	
 	User user;
 	
-	@GetMapping("/getuser")
-	public String getUser() {
-		return "Return User";
+	@PostMapping("/addname")
+	public String addName(@RequestParam String name) {
+		return name + " Added";
+	}
+	
+	@PostMapping("/addid")
+	public String addId(@RequestParam int id) {
+		return id + "ID Added";
+	}
+	
+	@GetMapping("/getname")
+	public String getName(@RequestParam(required = false, defaultValue = "Guest") String name) {
+		return "Hello" + name;
+	}
+	
+	@GetMapping("/getid")
+	public String getId(@RequestParam(required = false, defaultValue = "0") int id) {
+		return "User ID : " + id;
 	}
 	
 }
